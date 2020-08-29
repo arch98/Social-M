bc0aaa8a963820e50840a2e07f271719dac6cc50
Archana Chandrasekar <archanachandrasekar563@gmail.com>
2020-08-29 20:52:53 +0530

    login with redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import First from './Components/Entry/First.js';
 import Signup from './Components/Entry/Signup.js';
+import Account from './Components/Page/Account.js';
+import Chat from './Components/Interact/Chat.js';
+import Info from './Components/Interact/Info.js';
+import Chatter from './Components/Chat/Chatter.js';
+
 
 function App() {
   return (
       <Router>
         <Route exact path="/" component={First}/>
         <Route path ="/signup" component={Signup}/>
+        <Route path ="/Account" component={Account}/>
+        <Route path="/Chat" component={Chat}/>
+        <Route path ="/Info" component={Info}/>
+        <Route path ='/chatterbot' render ={(props) => (<Chatter {...props} name ='Chatterbot'/>)}/>
+        <Route path ='/dan' render ={(props) => (<Chatter {...props} name ='Dan'/>)}/>
+        <Route path ='/nancy' render ={(props) => (<Chatter {...props} name ='Nancy'/>)}/>
       </Router>
   );
 }
--- client/src/Components/Entry/Login.js
+++ client/src/Components/Entry/Login.js
@@ -1,75 +1,51 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import {Button,Form} from 'react-bootstrap';
 import '../../Style/Entry/Login.css';
-import {Link} from 'react-router-dom';
+import {Link,Redirect} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {name} from '../../actions/name';
+import {login} from '../../actions/index';
 
 
-class Login extends Component{
+function Login(){
 
-    constructor(props){
-        super(props);
+    const [user,setUser] = useState("");
+    const [pass,setPass] = useState("");
+    const[show,setShow] = useState(false);
+    const dispatch = useDispatch();
 
-        this.state ={
-            move:false,
-            user:"",
-            pass:""
-        }
-
-        this.userInputChange = this.userInputChange.bind(this);
-        this.send = this.send.bind(this);
+    const handleClick = () =>{
+        dispatch(name(user));
+        dispatch(login());
+        setUser("");
+        setPass("");
+        setShow(true);
     }
 
-    /* userInputChange is used to store all the changes made in the input form as a state */
-    userInputChange(event){
-        var t = event.target.id;
-        console.log(event.target.value);
-        if(t === "user"){
-            this.setState({
-                user:event.target.value
-            });
-        }else{
-            this.setState({
-                pass:event.target.value
-            });
-        }
-    }
-/*send function is used to retrieve the values on click of the submit button in the form*/
-    send(event){
-       const payload ={
-           "Username": this.state.user,
-           "Password":this.state.pass
-       }
-
-        this.setState({
-            move:true,
-            user:"",
-            pass:""
-        });
-    }
-
-    render(){
+       
         return(
             <div className ="Login">
                 <h3 id="title1">Login!</h3>
                 <Form>
                     <Form.Group>
                         <Form.Label><h3>Username</h3></Form.Label>
-                        <Form.Control id="user" onChange ={this.userInputChange} value ={this.state.user}/>
+                        <Form.Control id="user" onChange ={(event)=>{setUser(event.target.value)}} value = {user}/>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label><h3>Password</h3></Form.Label>
-                        <Form.Control id="pass" onChange ={this.userInputChange} value={this.state.pass}/>
+                        <Form.Control type ="password" id="pass" onChange ={(event)=>{setPass(event.target.value)}} value = {pass}/>
                     </Form.Group>
-                    <Button variant="primary" id="btn1" onClick ={this.send}>
+                    <Button variant="primary" id="btn1" onClick ={handleClick}>
                         Submit
                     </Button>
                     <div className="link1">
                         <Link to ={'/signup'}><h5>Don't have an account? Create one!</h5></Link>
                     </div>
                 </Form>
+                {show === true ? <Redirect to = {'/Account'}/> : null}
             </div>
         )
-    }
+    
 }
 
 export default Login;
